d97636d595a9ea78b6ad803e5aefebaa76a76bd6
ori benshushan <orbenshu@g.jct.ac.il>
2023-02-23 21:12:44 +0200

    OriChanges

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const pages = {
   Yummy: document.getElementById('Yummy-template').content.cloneNode(true),
   Desserts: document.getElementById('Desserts-template').content.cloneNode(true),
-  Saleds: document.getElementById('Saleds-template').content.cloneNode(true)
+  Saleds: document.getElementById('Saleds-template').content.cloneNode(true),
+  MainCourses: document.getElementById('MainCourses-template').content.cloneNode(true),
+  MyRecipes: document.getElementById('MyRecipes-template').content.cloneNode(true),
+  SignIn: document.getElementById('SignIn-template').content.cloneNode(true),
+  SignUp: document.getElementById('SignUp-template').content.cloneNode(true)
 };
 
 function showPage(id) {
-  const content = document.getElementById('content');
-  content.innerHTML = '';
+  const home = document.getElementById('home');
+  home.innerHTML = '';
   const page = pages[id];
-  content.appendChild(page);
-  const pageElement = content.querySelector('.page');
+  home.appendChild(page);
+  const pageElement = home.querySelector('.page');
   pageElement.classList.add('active');
 }
 
