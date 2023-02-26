87216d61f94594b4eb222ea8dcd0b8884a02b3be
ori benshushan <orbenshu@g.jct.ac.il>
2023-02-26 10:09:42 +0200

    scriptWork

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 const pages = {
-  Yummy: document.getElementById('Yummy-template').content.cloneNode(true),
-  Desserts: document.getElementById('Desserts-template').content.cloneNode(true),
-  Saleds: document.getElementById('Saleds-template').content.cloneNode(true),
-  MainCourses: document.getElementById('MainCourses-template').content.cloneNode(true),
-  MyRecipes: document.getElementById('MyRecipes-template').content.cloneNode(true),
-  SignIn: document.getElementById('SignIn-template').content.cloneNode(true),
-  SignUp: document.getElementById('SignUp-template').content.cloneNode(true)
+  Yummy: document.getElementById('Yummy-template').content,
+  Desserts: document.getElementById('Desserts-template').content,
+  Saleds: document.getElementById('Saleds-template').content,
+  MainCourses: document.getElementById('MainCourses-template').content,
+  MyRecipes: document.getElementById('MyRecipes-template').content,
+  SignIn: document.getElementById('SignIn-template').content,
+  SignUp: document.getElementById('SignUp-template').content
 };
 
 function showPage(id) {
   const content = document.getElementById('content');
   content.innerHTML = '';
   const page = pages[id];
-  content.appendChild(page);
+  content.appendChild(page.cloneNode(true));
   const pageElement = content.querySelector('.page');
   pageElement.classList.add('active');
 }
