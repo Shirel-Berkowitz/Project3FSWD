1eb301409b081921d16be04b88e5cbe7650de48f
Shirel <shkadosh@g.jct.ac.il>
2023-03-01 18:16:44 +0200

    GetFavoriteRecipes

--- userScript.js
+++ userScript.js
@@ -32,11 +32,26 @@ function AddFavoriteRecipes(id)
     if (user) {
         // Get the user object from localStorage using the username as the key
         let userObj = JSON.parse(localStorage.getItem(user.username));
-
         // Add the recipe ID to the user's favorite recipes array
         userObj.favoriteRecipes.push(id);
-
         // Save the updated user object to localStorage
         localStorage.setItem(user.username, JSON.stringify(userObj));
     }
 } 
+
+// Get all the favorite recipes of the logged-in user
+function GetFavoriteRecipes() {
+    // Get the logged-in user from localStorage
+    let user = JSON.parse(localStorage.getItem("userLoggedIn"));
+    // Check if a user is logged in
+    if (user) {
+      // Get the user object from localStorage using the username as the key
+      let userObj = JSON.parse(localStorage.getItem(user.username));
+      // Return the user's favorite recipes array
+      return userObj.favoriteRecipes;
+    }
+  
+    // If no user is logged in, return an empty array
+    return [];
+  }
+  
